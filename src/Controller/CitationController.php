24bf2e9698dcf5675b9f927de5db10c415f5f271
Jeff-NFS <jeff-nfs@MacBook-Air-de-Jeff.local>
2026-09-11 11:35:53 +0200

    feat: affichage de la bibliothèque et gestion de la liste vide

---
+++ src/Controller/CitationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CitationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CitationController extends AbstractController
+{
+    #[Route('/', name: 'app_citation_index', methods: ['GET'])]
+    public function index(CitationRepository $citationRepository): Response
+    {
+        return $this->render('citation/index.html.twig', [
+            'citations' => $citationRepository->findBy([], ['dateAjout' => 'DESC']),
+        ]);
+    }
+}
