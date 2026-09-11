c03d03b9734756020d9af3e8179823d131092afa
Jeff-NFS <jeff-nfs@MacBook-Air-de-Jeff.local>
2026-09-11 11:19:16 +0200

    chore: configuration de la base de données (SQLite) + Entity Citation

---
+++ src/Entity/Citation.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CitationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CitationRepository::class)]
+class Citation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $texte = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $auteur = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $source = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dateAjout = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $categorie = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $langue = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $note = null;
+
+    #[ORM\Column]
+    private ?bool $favori = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(string $texte): static
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getSource(): ?string
+    {
+        return $this->source;
+    }
+
+    public function setSource(?string $source): static
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    public function getDateAjout(): ?\DateTimeImmutable
+    {
+        return $this->dateAjout;
+    }
+
+    public function setDateAjout(\DateTimeImmutable $dateAjout): static
+    {
+        $this->dateAjout = $dateAjout;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): static
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getLangue(): ?string
+    {
+        return $this->langue;
+    }
+
+    public function setLangue(string $langue): static
+    {
+        $this->langue = $langue;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function isFavori(): ?bool
+    {
+        return $this->favori;
+    }
+
+    public function setFavori(bool $favori): static
+    {
+        $this->favori = $favori;
+
+        return $this;
+    }
+}
