85ec3e8403b755f09e0da17ff326f065d9440de8
Jeff-NFS <jeff-nfs@MacBook-Air-de-Jeff.local>
2026-09-11 11:44:33 +0200

    feat: page de consultation d'une citation

--- src/Controller/CitationController.php
+++ src/Controller/CitationController.php
@@ -6,6 +6,7 @@ use App\Repository\CitationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Citation;
 
 final class CitationController extends AbstractController
 {
@@ -14,6 +15,14 @@ final class CitationController extends AbstractController
     {
         return $this->render('citation/index.html.twig', [
             'citations' => $citationRepository->findBy([], ['dateAjout' => 'DESC']),
+        ]);
+    }
+
+    #[Route('/citation/{id}', name: 'app_citation_show', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function show(Citation $citation): Response
+    {
+        return $this->render('citation/show.html.twig', [
+            'citation' => $citation,
         ]);
     }
 }
